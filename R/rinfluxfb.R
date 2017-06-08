5eefd75c584f1f2896c117213a0db0fa0f6b3c74
martinbel <mbtangotan@gmail.com>
2017-06-08 16:55:06 -0300

    changes in description

--- R/rinfluxfb.R
+++ R/rinfluxfb.R
@@ -1,3 +1,10 @@
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
 #' @export
 #' @param response (httr::GET output)
 #' @param return_datatable (bool)
